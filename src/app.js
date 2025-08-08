4b83957456bea6fe4b6894890bd7744efa673ffe
Marcos flores <marcosrogf03@gmail.com>
2025-08-07 23:43:18 -0300

    fix env vulnerabilities

--- src/app.js
+++ src/app.js
@@ -2,15 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import taskRouter from "./routes/taskRouter.js";
-
-const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://marcosrogf03:itajunior2025oss@training.o3lgbvl.mongodb.net/";
+import cors from "cors";
 
 dotenv.config();
+const MONGO_URI = process.env.MONGO_URI 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
+app.use(cors())
 
 app.use("/tasks", taskRouter);
 
